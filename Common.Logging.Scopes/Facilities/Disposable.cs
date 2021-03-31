9b0e660c9155315510665781b47e4d33fb39a75c
Juanje <jespinosa@evicertia.com>
2021-03-31 12:59:14 +0200

    Common.Logging.NLog45 & Common.Logging.Scopes

---
+++ Common.Logging.Scopes/Facilities/Disposable.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Common.Logging.Scopes
+{
+	internal class Disposable : IDisposable
+	{
+		#region Fields
+
+		private readonly Action _disposeAction;
+
+		private bool _disposed;
+
+		#endregion
+
+		#region .ctors
+
+		public Disposable(Action disposeAction)
+		{
+			_disposeAction = disposeAction;
+		}
+
+		public Disposable(Action createAction, Action disposeAction)
+		{
+			createAction();
+			_disposeAction = disposeAction;
+		}
+
+		#endregion
+
+		#region Public methods
+
+		public static Disposable<T> Create<T>(Func<T> createAction, Action<T> disposeAction)
+		{
+			return new Disposable<T>(createAction, disposeAction);
+		}
+
+		public static Disposable<T> Using<T>(T instance, Action<T> disposeAction)
+		{
+			return new Disposable<T>(() => instance, disposeAction);
+		}
+
+		public static Disposable For(Action disposeAction)
+		{
+			return new Disposable(disposeAction);
+		}
+
+		public static Disposable For(Action createAction, Action disposeAction)
+		{
+			return new Disposable(createAction, disposeAction);
+		}
+
+		public void Dispose()
+		{
+			if (!_disposed && _disposeAction != null)
+			{
+				_disposed = true;
+				_disposeAction();
+			}
+		}
+
+		#endregion
+	}
+
+	internal class Disposable<T> : IDisposable
+	{
+		#region Fields & Properties
+
+		private bool _disposed;
+		private readonly T _instance;
+		private readonly Action<T> _disposeAction;
+
+		public T Instance => _instance;
+
+		#endregion
+
+		#region .ctors
+
+		public Disposable(Func<T> createAction, Action<T> disposeAction)
+		{
+			_instance = createAction();
+			_disposeAction = disposeAction;
+		}
+
+		#endregion
+
+		#region Public methods
+
+		public void Dispose()
+		{
+			if (!_disposed && _disposeAction != null)
+			{
+				_disposed = true;
+				_disposeAction(_instance);
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ Common.Logging.Scopes/Facilities/GenericExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Common.Logging.Scopes
+{
+	internal static class GenericExtensions
+	{
+		/// <summary>
+		/// Throws if null.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="source">The source.</param>
+		/// <param name="paramName">Name of the param.</param>
+		/// <returns></returns>
+		public static T ThrowIfNull<T>(this T source, string paramName)
+			where T : class
+		{
+			if (source == null)
+				throw new ArgumentNullException(paramName);
+
+			return source;
+		}
+
+		/// <summary>
+		/// Throws if source is null.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="source">The source.</param>
+		/// <param name="message">Name of the param.</param>
+		/// <returns></returns>
+		public static T ThrowIfNull<T>(this T source, string paramName, string message, params object[] args)
+			where T : class
+		{
+			if (source == null)
+				throw new ArgumentNullException(paramName, args != null ? string.Format(message, args) : message);
+
+			return source;
+		}
+	}
+}
