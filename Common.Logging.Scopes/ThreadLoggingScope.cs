f7773cb2bb57e4fdc55e71d4ea45823ecde5616c
Pablo Ruiz <pablo.ruiz@gmail.com>
2022-12-29 02:44:21 +0100

    Added a new SwallowExceptions opt-in property to ThreadLoggingScope so users can mitigate #5 until we find a proper fix.

--- Common.Logging.Scopes/ThreadLoggingScope.cs
+++ Common.Logging.Scopes/ThreadLoggingScope.cs
@@ -7,13 +7,15 @@ namespace Common.Logging.Scopes
 	public sealed class ThreadLoggingScope : IDisposable
 	{
 		#region Fields
-
+		private static readonly ILog _log = LogManager.GetLogger(typeof(ThreadLoggingScope));
 		private static readonly object _removeMarker = new object();
 
 		private readonly IDictionary<string, object> _variables = new Dictionary<string, object>();
 		private readonly IVariablesContext _context;
 
 		private bool _disposed;
+
+		public static bool SwallowExceptions = false; //< Used to allow configuring swallowing exceptions on unexpected error conditions.
 
 		#endregion
 
@@ -35,22 +37,36 @@ namespace Common.Logging.Scopes
 			Guard.IsNotNullNorEmpty(key, nameof(key));
 			Guard.Against<InvalidOperationException>(_disposed, "{0} already disposed?!", nameof(ThreadLoggingScope));
 
-			if (_variables.ContainsKey(key))
+			try
 			{
-				// Already saved a value for this variable..
-			}
-			else if (_context.Contains(key))
-			{
-				// Save value to restore on dispose..
-				_variables.Add(key, _context.Get(key));
-			}
-			else
-			{
-				// Value not present, so ensure removal on dispose..
-				_variables.Add(key, _removeMarker);
-			}
+				if (_variables.ContainsKey(key))
+				{
+					// Already saved a value for this variable..
+				}
+				else if (_context.Contains(key))
+				{
+					// Save value to restore on dispose..
+					_variables.Add(key, _context.Get(key));
+				}
+				else
+				{
+					// Value not present, so ensure removal on dispose..
+					_variables.Add(key, _removeMarker);
+				}
 
-			_context.Set(key, value);
+				_context.Set(key, value);
+			}
+			catch (Exception ex)
+			{
+				var message =
+					$"{{0}} unexpected exception while setting a thread scoped variable '{key}'. " +
+					$"This maybe related to issue https://github.com/evicertia/Common.Logging.Extras/issues/5, " +
+					$"which is an issue not easy to reproduce (and hence fix), but you can help us to fix this " +
+					$"issue faster by providing additional diagnostics details ;)";
+
+				if (SwallowExceptions) _log.WarnFormat(message, ex, "Ignoring");
+				else throw new InvalidOperationException(string.Format(message, "Caught"), ex);
+			}
 		}
 
 		public void Dispose()
